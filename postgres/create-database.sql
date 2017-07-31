3e58ad3e67719ddfae4b3546d5c29e7a3945690d
Vincent <vincent.sabourdy1@gmail.com>
2017-07-31 00:35:45 +0000

    Adding Gitlab

--- postgres/create-database.sql
+++ postgres/create-database.sql
@@ -8,3 +8,7 @@ GRANT ALL PRIVILEGES ON DATABASE mattermost TO mattermost;
 CREATE USER owncloud PASSWORD 'owncloud';
 CREATE DATABASE owncloud;
 GRANT ALL PRIVILEGES ON DATABASE owncloud TO owncloud;
+
+CREATE ROLE gitlab with LOGIN CREATEDB PASSWORD 'password';
+CREATE DATABASE gitlab;
+GRANT ALL PRIVILEGES ON DATABASE gitlab to gitlab;
