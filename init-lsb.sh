ca86086e838ede35bceaeecea38429a9be8445e9
Vincent <vincent.sabourdy1@gmail.com>
2017-08-11 10:45:56 +0200

    Updating starting process

--- init-lsb.sh
+++ init-lsb.sh
@@ -1,10 +1,23 @@
+function setProxyDomainName {
+        REVERSE_PROXY_DOMAIN_NAME=`hostname -i | cut -d' ' -f1`
+	export REVERSE_PROXY_DOMAIN_NAME=$REVERSE_PROXY_DOMAIN_NAME
+        echo "Reverse proxy domain name is \"$REVERSE_PROXY_DOMAIN_NAME\"" >&2
+}
+
+function setPassword {
+	read -s -p "Enter your password: " PASS
+	export PASS=$PASS
+}
+
 case "$1" in
     start)
         echo "Starting Docker Compose" >&2
-        echo "Reverse proxy domain name is \"$(hostname -i | cut -d' ' -f1)\"" >&2
-        echo "REVERSE_PROXY_DOMAIN_NAME=$(hostname -i | cut -d' ' -f1)" > .env
+
+	setProxyDomainName
+	setPassword
+
         docker-compose build
-        docker-compose up 
+        docker-compose up -d 
         ;;
 
     stop)
@@ -14,16 +27,20 @@ case "$1" in
 
     restart)
         echo "Restarting Docker Compose" >&2
-        docker-compose build
-        docker-compose restart
+        docker-compose restart -d
         ;;
 
     status)
         docker-compose ps
         ;;
 
+    up)
+	echo "Restarting service $2" >&2
+	setProxyDomainName
+	docker-compose up -d $2
+	;;
     *)
-        echo "Usage: $0 {start|stop|restart|status}" >&2
+	    echo "Usage: $0 {start|stop|restart|status|up [SERVICE] }" >&2
         exit 1
         ;;
 esac
